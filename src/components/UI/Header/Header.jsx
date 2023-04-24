71b126b816a0dd9ab53666077a0ce1780aab0718
ww_side <daniil.metelia@gmail.com>
2023-04-24 20:38:17 +0300

    update icons

--- src/components/UI/Header/Header.jsx
+++ src/components/UI/Header/Header.jsx
@@ -1,15 +1,15 @@
-import React from 'react';
-import logo from '../../../file/logo.png';
+import React from "react";
+import logo from "../../../file/logo.png";
 
 const Header = ({ isWhiteTheme, switchButton }) => {
   return (
-    <header className={`py-4 ${isWhiteTheme ? 'bg-white' : 'bg-zinc-900'}`}>
+    <header className={`py-4 ${isWhiteTheme ? "bg-white" : "bg-zinc-900"}`}>
       <nav className="flex justify-between px-6">
         <div className="flex items-center gap-2">
           <img src={logo} alt="favicon" />
           <h1
             className={`text-3xl font-bold ${
-              isWhiteTheme ? 'text-zinc-900' : 'text-white'
+              isWhiteTheme ? "text-zinc-900" : "text-white"
             }`}
           >
             My Board
